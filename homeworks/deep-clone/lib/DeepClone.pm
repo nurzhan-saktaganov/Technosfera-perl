1e05592c1fc847b5602c62ab0621301a3d184531
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-04 13:34:05 +0300

    DeepClone done

--- homeworks/deep-clone/lib/DeepClone.pm
+++ homeworks/deep-clone/lib/DeepClone.pm
@@ -3,6 +3,7 @@ package DeepClone;
 use 5.010;
 use strict;
 use warnings;
+no if $] >= 5.018, warnings => "experimental::smartmatch";
 
 =encoding UTF8
 
@@ -34,13 +35,52 @@ use warnings;
 
 =cut
 
+sub _clone_ {
+    my $orig = shift;
+    my $ref_map = shift;
+
+    if (!defined $orig) {
+        return undef;
+    } elsif (ref \$orig eq 'SCALAR') {
+        return $orig;
+    } elsif ($ref_map->{$orig}) {
+        return $ref_map->{$orig};
+    }
+
+    my $cloned;
+    my $type = ref $orig;
+
+    given ($type) {
+        when ('ARRAY') {
+            $cloned = [];
+            $ref_map->{$orig} = $cloned;
+            push @$cloned, _clone_($_, $ref_map) for @$orig;
+        }
+        when ('HASH') {
+            $cloned = {};
+            $ref_map->{$orig} = $cloned;
+            $cloned->{$_} = _clone_($orig->{$_}, $ref_map) for keys %$orig;
+        }
+        default {
+            die 'Not supported!';
+        }
+    }
+    return $cloned;
+}
+
 sub clone {
-	my $orig = shift;
-	my $cloned;
-	# ...
-	# deep clone algorith here
-	# ...
-	return $cloned;
+    my $orig = shift;
+    my $cloned;
+    # ...
+    # deep clone algorithm here
+    # ...
+    eval {
+        my $ref_map = {};
+        $cloned = _clone_($orig, $ref_map);
+    } or do {
+       $cloned = undef;
+    };
+    return $cloned;
 }
 
 1;
