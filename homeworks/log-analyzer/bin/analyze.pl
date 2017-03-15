f3607d41e0c2771ea8fef72b9962b1bc3e8c8da2
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-16 01:31:16 +0300

    fixing and refactoring

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -2,9 +2,8 @@
 
 use strict;
 use warnings;
-use Date::Parse;
-use Date::Format;
-use List::Util qw(reduce);
+use List::Util qw(sum0);
+use List::MoreUtils qw(uniq);
 
 
 my $filepath = $ARGV[0];
@@ -51,7 +50,9 @@ sub parse_file {
         my $compress_rate = $+{'compress_rate'};
 
         eval {
-            map {defined $_ ? 1 : die 'Not defined'} ($ip, $datetime, $code, $sent, $compress_rate);
+            for ($ip, $datetime, $code, $sent, $compress_rate) {
+               die 'Not defined' if not defined $_;
+            }
         };
         if ($@) {
             # warn $log_line;
@@ -66,10 +67,10 @@ sub parse_file {
         $compress_rate = 1.0 if $compress_rate eq '-';
         $compress_rate *= 1.0;
 
-        $datetime =~ s/:\d+ /:00 /;
+        $datetime =~ s/:\d\d \+0300//;
 
         push @{$statistics{$ip}}, {
-            'timestamp' => $datetime, # str2time($datetime),
+            'datetime' => $datetime,
             'code' => $code,
             'sent' => $sent,
             'compress_rate' => $compress_rate,
@@ -80,48 +81,41 @@ sub parse_file {
     # you can put your code here
     $result = {
         'statistics' => \%statistics,
-        'codes' => \%codes,
+        'codes' =>[keys %codes],
     };
 
     return $result;
 }
 
-sub uniq {
-    my %seen;
-    return grep {!$seen{$_}++} @_;
-}
-
 sub get_really_sent {
     my $array_ref = shift;
-    my @really_sent = grep {$_->{'code'} == 200} @$array_ref;
-    @really_sent = map {int($_->{'sent'} * $_->{'compress_rate'})} @really_sent;
-    my $data = @really_sent ? reduce {$a + $b} @really_sent : 0;
-    $data = int($data / 1024);
-    return $data;
+    return sum0
+             map {int($_->{'sent'} * $_->{'compress_rate'})}
+               grep {$_->{'code'} == 200} @$array_ref;
 }
 
 sub get_sent_for_code {
     my ($array_ref, $code) = @_;
-    my @sent = grep {$_->{'code'} == $code} @$array_ref;
-    @sent = map {$_->{'sent'}} @sent;
-    my $result = @sent ? reduce {$a + $b} @sent : 0;
-    $result = int($result / 1024);
-    return $result;
+    return sum0
+             map {$_->{'sent'}}
+               grep {$_->{'code'} == $code} @$array_ref;
 }
+
+
 
 sub report {
     my $result = shift;
 
     # you can put your code here
     my $statistics = $result->{'statistics'};
-    my @codes = sort keys %{$result->{'codes'}};
+    my @codes = sort @{$result->{'codes'}};
 
     my @report;
-    my @total_minutes;
+    my %total_minutes;
     for my $ip (keys %$statistics) {
         my $count = @{$statistics->{$ip}};
         my $data = get_really_sent($statistics->{$ip});
-        my @minutes = uniq map {$_->{'timestamp'}} @{$statistics->{$ip}};
+        my @minutes = uniq map {$_->{'datetime'}} @{$statistics->{$ip}};
         my $avg = $count / @minutes;
 
         my $ip_summary = {
@@ -135,25 +129,27 @@ sub report {
             $ip_summary->{$code} = get_sent_for_code($statistics->{$ip}, $code);
         }
 
-        push @total_minutes, @minutes;
+        $total_minutes{$_} = 1 for @minutes;
         push @report, $ip_summary;
     }
-    @total_minutes = uniq @total_minutes;
     @report = sort {$b->{'count'} <=> $a->{'count'}} @report;
     my $total = {'IP' => 'total'};
     for my $field ('count', 'data', @codes){
-        $total->{$field} = reduce {$a + $b} map {$_->{$field}} @report;
+        $total->{$field} = sum0 map {$_->{$field}} @report;
     }
-    my $avg = $total->{'count'} / @total_minutes;
+    my $avg = $total->{'count'} / keys %total_minutes;
     $total->{'avg'} = sprintf("%.2f", $avg);
     @report = @report[0..9];
 
     unshift @report, $total;
 
+    for my $row (@report) {
+        $row->{$_} = int($row->{$_} / 1024) for ('data', @codes);
+    }
+
     my @fields = ('IP', 'count', 'avg', 'data', @codes);
 
     print join("\t", @fields) . "\n";
-    # print "total\t22344\t544.98\t7375992\t1784676\t1108\t705\t85\t15469\t11269\t0\t1\t0\t514\n";
 
     for my $row (@report) {
         my @to_print;
