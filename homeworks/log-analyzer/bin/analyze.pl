d75aaa0d3f87fe887baac3b8ef25e62ea84b6db6
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-23 04:16:13 +0300

    log analyzer redone

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -19,22 +19,25 @@ sub parse_file {
 
     # you can put your code here
 
-    my $result;
+    my $result = {
+        "total" => {
+            "IP" => "total",
+            "minutes" => {},
+        },
+    };
 
-    my @line_components;
-    $line_components[0] = '(?<ip>(\d+\.){3}\d+)';
-    $line_components[1] = '\[(?<datetime>[^\]]+)\]';
-    $line_components[2] = '"(?<request>[^"]+)"';
-    $line_components[3] = '(?<code>\d+)';
-    $line_components[4] = '(?<sent>\d+)';
-    $line_components[5] = '"(?<refferer>[^"]+)"';
-    $line_components[6] = '"(?<user_agent>[^"]+)"';
-    $line_components[7] = '"(?<compress_rate>[\d\.-]+)"';
-
-    my $line_regex = '^' . join('\s*', @line_components) . '$';
-
-    my %statistics;
-    my %codes;
+    my $line_regex = qr/
+        ^
+        (?<ip>(\d+\.){3}\d+)\s
+        \[(?<datetime>[^\]]+)\]\s
+        "(?<request>[^"]+)"\s
+        (?<code>\d+)\s
+        (?<sent>\d+)\s
+        "(?<refferer>[^"]+)"\s
+        "(?<user_agent>[^"]+)"\s
+        "(?<compress_rate>[\d\.-]+)"
+        $
+    /x;
 
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
     while (my $log_line = <$fd>) {
@@ -43,117 +46,68 @@ sub parse_file {
         # $log_line contains line from log file
         $log_line =~ $line_regex;
 
+        eval {
+            for ('ip', 'datetime', 'code', 'sent', 'compress_rate'){
+                die 'Not defined' unless exists $+{$_};
+            }
+            1;
+        } or do {
+            warn 'Unexpected log format: ' . $log_line;
+            next;
+        };
+
         my $ip = $+{'ip'};
         my $datetime = $+{'datetime'};
         my $code = $+{'code'};
         my $sent = $+{'sent'};
         my $compress_rate = $+{'compress_rate'};
 
-        eval {
-            for ($ip, $datetime, $code, $sent, $compress_rate) {
-               die 'Not defined' if not defined $_;
-            }
-        };
-        if ($@) {
-            # warn $log_line;
-            next;
-        }
-
-        $codes{$code} = 1;
-
-        $statistics{$ip} ||= [];
-        $code = int($code);
-        $sent = int($sent);
-        $compress_rate = 1.0 if $compress_rate eq '-';
-        $compress_rate *= 1.0;
-
         $datetime =~ s/:\d\d \+0300//;
+        $compress_rate = 1.0 if $compress_rate eq '-';
+        $result->{$ip} ||= {};
 
-        push @{$statistics{$ip}}, {
-            'datetime' => $datetime,
-            'code' => $code,
-            'sent' => $sent,
-            'compress_rate' => $compress_rate,
-        };
+        # ip stats
+        $result->{$ip}{'IP'} //= $ip;
+        $result->{$ip}{'minutes'}{$datetime} = 1;
+        $result->{$ip}{$code} += $sent;
+        $result->{$ip}{'data'} += int($sent * $compress_rate) if $code eq '200';
+        ++$result->{$ip}{'count'};
+
+        # total stats
+        $result->{'total'}{'minutes'}{$datetime} = 1;
+        $result->{'total'}{$code} += $sent;
+        $result->{'total'}{'data'} += int($sent * $compress_rate) if $code eq '200';
+        ++$result->{'total'}{'count'};
     }
     close $fd;
 
     # you can put your code here
-    $result = {
-        'statistics' => \%statistics,
-        'codes' =>[keys %codes],
-    };
-
+    for my $row (values %$result) {
+        $row->{'minutes'} = keys %{$row->{'minutes'}};
+    }
     return $result;
 }
-
-sub get_really_sent {
-    my $array_ref = shift;
-    return sum0
-             map {int($_->{'sent'} * $_->{'compress_rate'})}
-               grep {$_->{'code'} == 200} @$array_ref;
-}
-
-sub get_sent_for_code {
-    my ($array_ref, $code) = @_;
-    return sum0
-             map {$_->{'sent'}}
-               grep {$_->{'code'} == $code} @$array_ref;
-}
-
-
 
 sub report {
     my $result = shift;
 
     # you can put your code here
-    my $statistics = $result->{'statistics'};
-    my @codes = sort @{$result->{'codes'}};
+    my @table = sort {$b->{'count'} <=> $a->{'count'}} values %$result;
+    my @codes = sort grep {$_ =~ m/\d+/} keys %{$result->{'total'}};
+    my @headers = ('IP', 'count', 'avg', 'data', @codes);
 
-    my @report;
-    my %total_minutes;
-    for my $ip (keys %$statistics) {
-        my $count = @{$statistics->{$ip}};
-        my $data = get_really_sent($statistics->{$ip});
-        my @minutes = uniq map {$_->{'datetime'}} @{$statistics->{$ip}};
-        my $avg = $count / @minutes;
-
-        my $ip_summary = {
-            'IP' => $ip,
-            'count' => $count,
-            'avg' => sprintf("%.2f", $avg),
-            'data' => $data,
-        };
-
-        for my $code (@codes) {
-            $ip_summary->{$code} = get_sent_for_code($statistics->{$ip}, $code);
-        }
-
-        $total_minutes{$_} = 1 for @minutes;
-        push @report, $ip_summary;
-    }
-    @report = sort {$b->{'count'} <=> $a->{'count'}} @report;
-    my $total = {'IP' => 'total'};
-    for my $field ('count', 'data', @codes){
-        $total->{$field} = sum0 map {$_->{$field}} @report;
-    }
-    my $avg = $total->{'count'} / keys %total_minutes;
-    $total->{'avg'} = sprintf("%.2f", $avg);
-    @report = @report[0..9];
-
-    unshift @report, $total;
-
-    for my $row (@report) {
-        $row->{$_} = int($row->{$_} / 1024) for ('data', @codes);
+    if (@table < 2) {
+        return;
     }
 
-    my @fields = ('IP', 'count', 'avg', 'data', @codes);
+    @table = @table[0..10];
 
-    print join("\t", @fields) . "\n";
+    print join("\t", @headers) . "\n";
 
-    for my $row (@report) {
-        my @to_print;
-        push @to_print, $row->{$_} for @fields;
-        print join("\t", @to_print) . "\n";
+    for my $row (@table) {
+        $row->{$_} //= 0 and $row->{$_} >>= 10 for ('data', @codes);
+        my $avg = $row->{'count'} / $row->{'minutes'};
+        $row->{'avg'} = sprintf("%.2f", $avg);
+        print join("\t", map {$row->{$_}} @headers) . "\n";
     }
 }
