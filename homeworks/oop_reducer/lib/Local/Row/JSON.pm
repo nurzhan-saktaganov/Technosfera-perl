51770f2621fdf6a6b7fc75590b17073327c8e714
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-25 06:04:34 +0300

    oop_reducer: Local::Row::JSON

---
+++ homeworks/oop_reducer/lib/Local/Row/JSON.pm
@@ -0,0 +1,46 @@
+package Local::Row::JSON;
+use parent qw/Local::Row/;
+
+use strict;
+use warnings;
+use JSON qw/decode_json/;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Row - base abstract row
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+sub _init {
+    my ($self, %args) = @_;
+    unless (exists $args{'src'}) {
+        die '"src" param required';
+    }
+    if (ref $args{'src'}) {
+        return undef;
+    }
+    my $json;
+    eval {
+        $json = decode_json($args{'src'});
+        die if ref $json ne 'HASH';
+        1;
+    } or do {
+        return undef;
+    };
+    bless $json, ref $self;
+    return $json;
+}
+
+1;
