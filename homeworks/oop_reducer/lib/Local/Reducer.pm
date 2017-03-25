1a8a9a2dfeaba669a376642a2aac610fc2b5419f
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-25 14:15:06 +0300

    hw oop_reducer: Local::Reducer::Sum

--- homeworks/oop_reducer/lib/Local/Reducer.pm
+++ homeworks/oop_reducer/lib/Local/Reducer.pm
@@ -3,6 +3,10 @@ package Local::Reducer;
 use strict;
 use warnings;
 
+use Scalar::Util qw/blessed/;
+use Local::Source;
+use Local::Row;
+
 =encoding utf8
 
 =head1 NAME
@@ -20,5 +24,55 @@ our $VERSION = '1.00';
 =head1 SYNOPSIS
 
 =cut
+
+sub new {
+    my ($class, @args) = @_;
+    my $self = bless {}, $class;
+    return $self->_init(@args);
+}
+
+sub _init {
+    my ($self, %args) = @_;
+    unless (exists $args{'source'}) {
+        die '"source" param required';
+    }
+    unless (exists $args{'row_class'}) {
+        die '"row_class" param required';
+    }
+
+    unless (exists $args{'initial_value'}) {
+        die '"initial_value" param required';
+    }
+
+    eval {
+        blessed($args{'source'}) and $args{'source'}->isa('Local::Source');
+    } or do {
+        die '"source" param must be an Local::Source instance';
+    };
+
+    eval {
+        $args{'row_class'}->isa('Local::Row');
+    } or do {
+        die '"row_class" param must be an Local::Row class';
+    };
+
+    $self->{'source'} = $args{'source'};
+    $self->{'row_class'} = $args{'row_class'};
+    $self->{'reduced'} = $args{'initial_value'};
+
+    return $self;
+}
+
+sub reduce_n {
+    return undef;
+}
+
+sub reduce_all {
+    return undef;
+}
+
+sub reduced {
+    return undef;
+}
 
 1;
---
+++ homeworks/oop_reducer/lib/Local/Reducer/Sum.pm
@@ -0,0 +1,77 @@
+package Local::Reducer::Sum;
+use parent qw/Local::Reducer/;
+
+use Scalar::Util qw(looks_like_number);
+
+use strict;
+use warnings;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Reducer::Sum
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+sub _init {
+    my ($self, %args) = @_;
+    
+    $self->SUPER::_init(%args);
+
+    unless (exists $args{'field'}) {
+        die '"field" param required';
+    }
+
+    if (ref $args{'field'}) {
+        die '"field" param must be an scalar';
+    }
+
+    $self->{'field'} = $args{'field'};
+    return $self;
+}
+
+sub reduce_n {
+    my ($self, $n) = @_;
+    for (1..$n) {
+        my $src = $self->{'source'}->next();
+        last unless defined $src;
+        my $row = $self->{'row_class'}->new(src => $src);
+        next unless defined $row;
+        my $value = $row->get($self->{'field'}, 0);
+        next unless looks_like_number($value);
+        $self->{'reduced'} += $value;
+    }
+    return $self->{'reduced'};
+}
+
+sub reduce_all {
+    my $self = shift;
+    while (1) {
+        my $src = $self->{'source'}->next();
+        last unless defined $src;
+        my $row = $self->{'row_class'}->new(src => $src);
+        next unless defined $row;
+        my $value = $row->get($self->{'field'}, 0);
+        next unless looks_like_number($value);
+        $self->{'reduced'} += $value;
+    }
+    return $self->{'reduced'};
+}
+
+sub reduced {
+    my $self = shift;
+    return $self->{'reduced'};
+}
+
+1;
