25fd8732aecba1c92e0b7a8b33935f8db28bd19e
Nurzhan Saktaganov <nur92world@mail.ru>
2017-03-28 01:45:40 +0300

    hw save_stdin done

--- homeworks/save_stdin/bin/stdin.pl
+++ homeworks/save_stdin/bin/stdin.pl
@@ -4,5 +4,47 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-...
+sub main {
+    my $file;
+    GetOptions("file:s", \$file);
+    die '--file options required' unless defined $file;
+    open(my $fh, '>', $file)
+            or die "Cannot open file: $file";
 
+    STDIN->autoflush(1);
+    STDERR->autoflush(1);
+    $fh->autoflush(1);
+
+    my ($lines, $symbols, $avg_lentgh) = (0, 0, 0);
+
+    my $double_sigint = 0;
+
+    $SIG{'INT'} = sub {
+        if ($double_sigint) {
+            # close(STDIN);
+            # return;
+            $avg_lentgh = $symbols / $lines if $lines > 0;
+            print "$symbols $lines $avg_lentgh\n";
+            exit;
+        }
+        print STDERR "Double Ctrl+C for exit";
+        $double_sigint = 1;
+    };
+
+    print "Get ready\n";
+    while (<>){
+        $double_sigint = 0;
+        print $fh $_;
+        ++$lines;
+        chomp;
+        $symbols += length $_;
+    }
+    $SIG{'INT'} = 'DEFAULT';
+    $avg_lentgh = $symbols / $lines if $lines > 0;
+    print "$symbols $lines $avg_lentgh\n";
+}
+
+# Yes, Python like style :)
+if (__PACKAGE__ eq 'main') {
+    main;
+}
