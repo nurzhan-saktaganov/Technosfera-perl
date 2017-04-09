86bf9535f3d5daeecc37a160603175e4896ded38
Nurzhan Saktaganov <nur92world@mail.ru>
2017-04-09 05:37:13 +0300

    just style change

--- homeworks/save_stdin/bin/stdin.pl
+++ homeworks/save_stdin/bin/stdin.pl
@@ -20,15 +20,14 @@ sub main {
     my $double_sigint = 0;
 
     $SIG{'INT'} = sub {
-        if ($double_sigint) {
-            # close(STDIN);
-            # return;
-            $avg_lentgh = $symbols / $lines if $lines > 0;
-            print "$symbols $lines $avg_lentgh\n";
-            exit;
+        unless ($double_sigint) {
+            $double_sigint++;
+            print STDERR "Double Ctrl+C for exit";
+            return;
         }
-        print STDERR "Double Ctrl+C for exit";
-        $double_sigint = 1;
+        $avg_lentgh = $symbols / $lines if $lines > 0;
+        print "$symbols $lines $avg_lentgh\n";
+        exit;
     };
 
     print "Get ready\n";
